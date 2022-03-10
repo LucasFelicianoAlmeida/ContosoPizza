7e52173d36fc80ddc67dcf1902f6a1a0eeafd28d
LucasFelicianoAlmeida <lucasfeliciano326@gmail.com>
2022-03-10 14:13:47 -0300

    changing pizza class

--- ContosoPizza/Models/Pizza.cs
+++ ContosoPizza/Models/Pizza.cs
@@ -1,8 +1,9 @@
-namespace ContosoPizza.Models;
-
-public class Pizza
+namespace ContosoPizza.Models
 {
-        public int Id {get; set;}
-        public string Name {get; set;}
-        public bool IsGlutenFree {get;set;}
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
 }
